0a727bb8ad29175f723e227ff2ad20412bf4636c
Arthur Gehlen <1arthurgehlen@gmail.com>
2026-04-07 16:11:51 -0300

    Adicionando supabase :)

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,10 +1,30 @@
-import styles from "./page.module.css";
+"use client";
+import styles from "../layout.module.css"
 
 const page = () => {
+  const handle_submit = (e) => {
+    e.preventDefault();
+    console.log('teste');
+  }
+
   return (
-    <div>
-      <h2>Login</h2>
-    </div>
+    <>
+      <h1 className={styles.auth_title}>Login</h1>
+
+      <form onSubmit={handle_submit} className={styles.form}>
+        <div className={styles.input_wrapper}>
+          <label className={styles.input_label} htmlFor="email">Email</label>
+          <input className={styles.input_container} type="email" id="email" placeholder="Digite o seu email" />
+        </div>
+
+        <div className={styles.input_wrapper}>
+          <label className={styles.input_label} htmlFor="password">Senha</label>
+          <input className={styles.input_container} type="password" id="password" placeholder="Digite sua senha" />
+        </div>
+
+        <button type="submit" className={styles.submit_btn}>Entrar</button>
+      </form>
+    </>
   );
 };
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+"use client";
+import { createClient } from "@/_lib/supabase/client";
 import Link from "next/link";
+import { useEffect, useMemo } from "react";
 import styles from "./page.module.css";
 
 const page = () => {
+  const supabase = useMemo(() => createClient(), []);
+
+  useEffect(() => {
+    const fetch_data = async () => {
+      const { data, error } = await supabase
+        .from("civilizations")
+        .select("*");
+
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(data);
+      }
+    };
+
+    fetch_data();
+  }, [supabase]);
+
   return (
     <div className={styles.page}>
-      <h1>
+      <h1 className={styles.main_title}>
         Bem-Vindo ao <span className={styles.civ_style}>Civ</span>Log
       </h1>
       <Link className={styles.link_to_login} href="/login">
